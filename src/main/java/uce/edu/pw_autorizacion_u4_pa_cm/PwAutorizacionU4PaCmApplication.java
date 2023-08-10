3b6eb5ebf23d1123a33878b6ca4230d5b3124509
paulo <aldazpaulo@gmail.com>
2023-08-09 19:47:20 -0500

    taller 38

---
+++ src/main/java/uce/edu/pw_autorizacion_u4_pa_cm/PwAutorizacionU4PaCmApplication.java
@@ -0,0 +1,13 @@
+package uce.edu.pw_autorizacion_u4_pa_cm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4PaCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4PaCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uce/edu/pw_autorizacion_u4_pa_cm/PwAutorizacionU4PaCmApplicationTests.java
@@ -0,0 +1,13 @@
+package uce.edu.pw_autorizacion_u4_pa_cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionU4PaCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
